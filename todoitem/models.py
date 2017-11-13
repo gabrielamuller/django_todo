59d28eb6c3ab15e89903884b06303f13c0704cc9
Gabriela Muller <mullergabrielaa@gmail.com>
2017-11-13 15:08:26 +0000

    Models changes

--- todoitem/models.py
+++ todoitem/models.py
@@ -7,4 +7,6 @@ class TodoItem(models.Model):
     priority = models.CharField(max_length=6, blank=False)
     
     def __str__(self):
-        return self.name
+        status = "D" if self.done else "N"
+        
+        return "{0} ({1})".format(self.name, status)
